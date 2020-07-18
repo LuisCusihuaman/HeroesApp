00020543f1fb2246f3c9a9a76e7b61f3fa5f5475
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-18 17:29:11 -0300

    chore: redirect loginscreen button with history router prop

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar } from "../components/ui/Navbar";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 export const AppRouter = () => {
