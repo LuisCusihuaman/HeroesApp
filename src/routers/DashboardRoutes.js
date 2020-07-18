bed9241c0eef74ae80227cf37ccd2bf5ea6c6013
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-18 15:33:48 -0300

    feat: app routes with principal routes

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar } from "../components/ui/Navbar";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { HeroScreen } from "../components/heroes/HeroScreen";
+import { DcScreen } from "../components/dc/DcScreen";
+
+export const DashboardRoutes = () => {
+	return (
+		<>
+			<Navbar />
+			<div>
+				<Switch>
+					<Route
+						exact
+						path="/marvel"
+						component={MarvelScreen}
+					></Route>
+					<Route
+						exact
+						path="/heroe/:heroeId"
+						component={HeroScreen}
+					></Route>
+					<Route exact path="/dc" component={DcScreen}></Route>
+					<Redirect to="/marvel" />
+				</Switch>
+			</div>
+		</>
+	);
+};
