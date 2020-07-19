5bb0656dd0d384ce99f3bcb6d91e128e2594bfe5
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-18 21:08:39 -0300

    chore: auth reducer and dispatch to pass data across the app

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { AppRouter } from "./routers/AppRouter";
+import { AuthContext } from "./auth/authContext";
+import { authReducer } from "./auth/authReducer";
+
+const init = () =>
+	JSON.parse(localStorage.getItem("user")) || { logged: false };
 
 export const HeroesApp = () => {
+	const [user, dispatch] = useReducer(authReducer, {}, init);
 	return (
-		<div>
+		<AuthContext.Provider value={{ user, dispatch }}>
 			<AppRouter />
-		</div>
+		</AuthContext.Provider>
 	);
 };
