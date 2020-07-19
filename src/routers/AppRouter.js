79b6131c9ddd683fec00ad33faa0c46e54a6542c
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-18 21:41:33 -0300

    chore: private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { AuthContext } from "../auth/authContext";
 export const AppRouter = () => {
+	const { user } = useContext(AuthContext);
+	console.log(user);
 	return (
 		<Router>
 			<div>
 				<Switch>
 					<Route exact path="/login" component={LoginScreen} />
-					<Route path="/" component={DashboardRoutes} />
+					<PrivateRoute
+						path="/"
+						component={DashboardRoutes}
+						isAuthenticated={user.logged}
+					/>
 				</Switch>
 			</div>
 		</Router>
