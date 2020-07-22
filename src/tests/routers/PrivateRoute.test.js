55cb2291f2780db2381de2d710e5686bc416d4e1
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-22 01:49:41 -0300

    test:  should lock the component if it is not authenticated

--- src/tests/routers/PrivateRoute.test.js
+++ src/tests/routers/PrivateRoute.test.js
@@ -3,12 +3,12 @@ import { mount } from "enzyme";
 import { PrivateRoute } from "../../routers/PrivateRoute";
 import { MemoryRouter } from "react-router-dom";
 describe("Pruebas en <PrivateRoute/>", () => {
+	const props = {
+		location: {
+			pathname: "/marvel",
+		},
+	};
 	test("debe de mostrar el componente si esta autenticado y guardar en localStorage", () => {
-		const props = {
-			location: {
-				pathname: "/marvel",
-			},
-		};
 		Storage.prototype.setItem = jest.fn();
 		const wrapper = mount(
 			<MemoryRouter>
@@ -20,6 +20,22 @@ describe("Pruebas en <PrivateRoute/>", () => {
 			</MemoryRouter>
 		);
 		expect(wrapper.find("span").exists()).toBe(true);
+		expect(localStorage.setItem).toHaveBeenCalledWith(
+			"lastPath",
+			props.location.pathname
+		);
+	});
+	test("debe de bloquear el componente si no esta autenticado", () => {
+		const wrapper = mount(
+			<MemoryRouter>
+				<PrivateRoute
+					isAuthenticated={false}
+					component={() => <span>Listo</span>}
+					{...props}
+				/>
+			</MemoryRouter>
+		);
+		expect(wrapper.find("span").exists()).toBe(false);
 		expect(localStorage.setItem).toHaveBeenCalledWith(
 			"lastPath",
 			props.location.pathname
